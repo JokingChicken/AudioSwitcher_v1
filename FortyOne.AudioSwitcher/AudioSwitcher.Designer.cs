b5d159151078979eb7b4c8ca885a4ecf79974255
JokingChicken <danj@axoid.net>
2021-06-27 16:49:20 +0200

    removed accedental code

--- FortyOne.AudioSwitcher/AudioSwitcher.Designer.cs
+++ FortyOne.AudioSwitcher/AudioSwitcher.Designer.cs
@@ -649,7 +649,6 @@
             this.dataGridView1.ShowRowErrors = false;
             this.dataGridView1.Size = new System.Drawing.Size(255, 269);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // deviceNameDataGridViewTextBoxColumn
             // 
